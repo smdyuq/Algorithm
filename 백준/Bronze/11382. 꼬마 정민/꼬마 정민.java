47a342f978d4c371a87033949de9bb8e74945ab5
smdyuq <129133645+smdyuq@users.noreply.github.com>
2023-12-22 16:03:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long num1 = sc.nextLong();
+
+		long num2 = sc.nextLong();
+
+		long num3 = sc.nextLong();
+
+		System.out.print(num1 + num2 + num3);
+
+	}
+}
